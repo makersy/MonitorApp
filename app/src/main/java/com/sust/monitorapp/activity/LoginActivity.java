f061fcd4e490a5e701d35844e1f741464ab3a426
yhl <1352605699@qq.com>
2020-03-17 13:56:26 +0800

    finish: save password local

--- app/src/main/java/com/sust/monitorapp/activity/LoginActivity.java
+++ app/src/main/java/com/sust/monitorapp/activity/LoginActivity.java
@@ -2,12 +2,15 @@ package com.sust.monitorapp.activity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sust.monitorapp.R;
@@ -44,6 +47,14 @@ public class LoginActivity extends AppCompatActivity {
     Button logInButton;
     @BindView(R.id.to_sign_in_button)
     Button toSignInButton;
+    @BindView(R.id.cb_remember_username)
+    CheckBox cbRememberUsername;
+    @BindView(R.id.cb_remember_password)
+    CheckBox cbRememberPassword;
+    @BindView(R.id.to_forget_reset_pwd)
+    TextView toForgetResetPwd;
+
+    private SharedPreferences sp;
 
 
     @Override
@@ -51,9 +62,71 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        initView();
 
     }
 
+    //初始化activity
+    private void initView() {
+        //从本地获取保存的数据
+        getLocal();
+    }
+
+    /**
+     * 记住用户名和密码
+     *
+     * @param saveAll 是否同时记住二者。false：只记住用户名
+     */
+    private void saveLocal(boolean saveAll) {
+        sp = this.getSharedPreferences("userinfo", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        //保存用户名和密码
+        editor.putString("username", username);
+        if (saveAll) {
+            //保存全部
+            //修改记录状态
+            editor.putBoolean("remember_username", true);
+            editor.putBoolean("remember_password", true);
+            //保存数据
+            editor.putString("password", password);
+        } else {
+            //只保存用户名
+            editor.putBoolean("remember_username", true);
+            editor.putBoolean("remember_password", false);
+            editor.putString("password", "");
+        }
+        editor.apply();
+    }
+
+    /**
+     * 从本地获取记住的用户名和密码
+     */
+    private void getLocal() {
+        sp = this.getSharedPreferences("userinfo", MODE_PRIVATE);
+
+        ename.setText(sp.getString("username", ""));
+        epassword.setText(sp.getString("password", ""));
+
+        if (sp.getBoolean("remember_password", false)) {
+            //如果上次选中记住密码
+            cbRememberPassword.setChecked(true);
+        }
+        if (sp.getBoolean("remember_username", false)){
+            //如果上次选中记住用户名
+            cbRememberUsername.setChecked(true);
+        }
+    }
+
+    /**
+     * 清空本地缓存
+     */
+    private void clearLocal() {
+        sp = this.getSharedPreferences("userinfo", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.clear();
+        editor.apply();
+    }
 
     //登录按钮点击事件
     @OnClick(R.id.log_in_button)
@@ -80,9 +153,18 @@ public class LoginActivity extends AppCompatActivity {
                     MyResponse myResponse = JsonUtil.jsonToBean(response.body().string(), MyResponse.class);
 
                     if (StringUtils.equals(myResponse.getStatusCode(), ResponseCode.LOGIN_SUCCESS.getCode())) {
-                        //登录成功，跳转
-                        User user = JsonUtil.jsonToBean(myResponse.getData().toString(), User.class);
-                        MyApplication.user = user;
+                        //登录成功
+                        //保存当前登录信息
+                        MyApplication.user = JsonUtil.jsonToBean(myResponse.getData(), User.class);
+                        //如果选择了记住密码/记住用户名选项，保存至本地
+                        if (cbRememberPassword.isChecked()) {
+                            saveLocal(true);
+                        }else if (cbRememberUsername.isChecked()) {
+                            saveLocal(false);
+                        } else {
+                            clearLocal();
+                        }
+                        //跳转
                         Intent intent = new Intent(LoginActivity.this, AdminMainActivity.class);
                         startActivity(intent);
                         finish();
@@ -90,7 +172,7 @@ public class LoginActivity extends AppCompatActivity {
                         //密码错误
                         Toast.makeText(LoginActivity.this, ResponseCode.PASSWORD_WRONG.getMsg(), Toast.LENGTH_SHORT).show();
                         handler.sendEmptyMessage(0);
-                    } else if (ResponseCode.USER_NOT_EXIST.equals(myResponse.getStatusCode())) {
+                    } else if (ResponseCode.USER_NOT_EXIST.getCode().equals(myResponse.getStatusCode())) {
                         //用户不存在
                         Toast.makeText(LoginActivity.this, ResponseCode.USER_NOT_EXIST.getMsg(), Toast.LENGTH_SHORT).show();
                         handler.sendEmptyMessage(0);
